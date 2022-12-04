c3085ebead6a5f2e92eb20e8f1ecc44a35f53945
akshaykumar <akshaykumar8734@gmail.com>
2022-12-04 19:14:34 +0530

    multiple user data with object

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-
+var ul = document.getElementById("users");
 function addUserData(event){
     event.preventDefault();
-    console.log(event);
+    //console.log(event);
     const name = event.target.name.value;
     const email = event.target.email.value;
 
@@ -11,5 +11,48 @@ function addUserData(event){
         email: email 
     };
     const serialized_data = JSON.stringify(obj);
-    localStorage.setItem("user_data", serialized_data);
+    localStorage.setItem("user_data" + email, serialized_data);
+    for(let o in localStorage){
+        if(o.startsWith("user_data")){
+            console.log(localStorage[o]);
+        }
+    }
+    addNewItem(obj);
+}
+
+function addNewItem(object) {
+  var ul = document.getElementById("users");
+  var li = document.createElement("li");
+  li.appendChild(
+    document.createTextNode(object.email)
+  ); // li.appendChild()
+
+  console.log(document.createElement("i"));
+  var a1 = document.createElement("input");
+  a1.id = "yash";
+  a1.type = "button";
+  a1.value = "Edit";
+  a1.addEventListener("click", function () {
+    console.log(object);
+    document.getElementById("name").value = object.name;
+    document.getElementById("email").value = object.email;
+    li.remove();
+  });
+  a1.className = "delete";
+  a1.style.border = "2px solid green";
+  
+  li.appendChild(a1);
+  var a = document.createElement("input");
+  a.type = "button";
+  a.value = "delete";
+  a.addEventListener("click", function () {
+    localStorage.removeItem("user_data" + object.email); // axios.delete(${apiendpoint}/registeruser/${object._id});
+
+    li.remove();
+  });
+  a.className = "delete";
+  a.style.border = "2px solid red";
+ 
+  li.appendChild(a);
+  ul.appendChild(li);
 }
