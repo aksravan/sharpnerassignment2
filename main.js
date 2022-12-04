86f7400030c6eabad7947e0c4053b6ce93855ef1
akshaykumar <akshaykumar8734@gmail.com>
2022-12-04 18:45:02 +0530

    user data with object

--- main.js
+++ main.js
@@ -5,5 +5,11 @@ function addUserData(event){
     const name = event.target.name.value;
     const email = event.target.email.value;
 
-    localStorage.setItem(email, name);
+    // localStorage.setItem(email, name);
+    var obj = {
+        name: name,
+        email: email 
+    };
+    const serialized_data = JSON.stringify(obj);
+    localStorage.setItem("user_data", serialized_data);
 }
